e342be464618fe7865fc71213396f1e7e2af4cbb
wujinli <wujinli>
2020-12-07 18:01:10 +0800

    other:添加请求地址

--- lib/business/constant.dart
+++ lib/business/constant.dart
@@ -1,14 +1,24 @@
-
-
-
-final String HF_API_KEY='52dc04b1b08a43dc9f51325a5b5cb87b';
-final String HF_API_BASE_URL='https://geoapi.qweather.com/';
-
+final String HF_API_KEY = '52dc04b1b08a43dc9f51325a5b5cb87b';
+final String HF_API_BASE_URL = 'https://geoapi.qweather.com/';
 
 //实况天气
-final String HF_NOW='v7/weather/now?';
+final String HF_NOW = 'v7/weather/now?';
 
-
-// https://geoapi.qweather.com/v2/city/lookup?location=tianjin&key=52dc04b1b08a43dc9f51325a5b5cb87b
-
-//https://geoapi.qweather.com/v2/city/top?key=52dc04b1b08a43dc9f51325a5b5cb87b
+/****************************玩安卓**********************************/
+final String WAN_ANDROID_BASE_URL = 'https://www.wanandroid.com/';
+final String BANNER_URL = 'banner/json'; //轮播
+final String TOP_URL = 'article/top/json'; // 置顶文章
+final String ARTICLE_DETAIL_URL = 'article/list/$pageNum/json'; // 文章
+final String TREE_URL = 'tree/json'; // 项目分类
+final String PROJECT_URL = 'project/tree/json'; //体系分类
+final String NAVI_URL = 'navi/json'; // 导航
+final String WXARTICLE_URL = 'wxarticle/chapters/json'; // 公众号分类
+final String WXARTICLE_DETAIL_URL = 'wxarticle/list/$id/$pageNum/json'; // 公众号文章
+final String HOTKEY_URL = 'hotkey/json'; // 搜索热门记录
+final String QUERY_URL = 'article/query/$pageNum/json'; // 搜索结果
+final String COLLECT_LIST_URL = 'lg/collect/list/$pageNum/json'; // 收藏列表
+final String COLLECT_URL = 'lg/collect/$id/json'; // 收藏
+final String UNCOLLECT_URL = 'lg/uncollect_originId/$id/json'; // 取消收藏
+final String COIN_URL = 'lg/coin/getcount/json'; // 个人积分
+final String COIN_LIST_URL = 'lg/coin/list/$pageNum/json'; // 我的积分记录
+final String COIN_RANK_URL = 'coin/rank/$pageNum/json'; // 我的积分记录
